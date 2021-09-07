91c69f557ba9c150cc71dedd2b3510c97a6ff529
wuzhengjun <wuzhengjun@smartahc.com>
2021-09-07 21:58:44 +0800

    新增商品分类页面布局

---
+++ src/components/goods/Cate.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图区域 -->
+    <el-card>
+        <el-row>
+            <el-col>
+                <el-button type="primary">添加分类</el-button>
+            </el-col>
+        </el-row>
+
+        <!-- 表格 -->
+
+        <!-- 分页 -->
+
+    </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return {}
+    },
+created(){},
+methods:{}
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
